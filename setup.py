9fbae6bf9155824ece4798320d84ce9f933f5b03
Tanmay Talekar <t.tanmay0307@gmail.com>
2026-09-12 10:52:37 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+setup(
+    name='ml-project',
+    version='0.0.1',
+    author='Tanmay',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
